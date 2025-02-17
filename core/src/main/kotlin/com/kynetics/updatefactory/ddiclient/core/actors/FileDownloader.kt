9391c7be17a5965b71e3351b2976a695746a11e1
Saurabh Mani <Saurabh@inevitableinfotech.com>
2025-02-17 12:37:28 +0530

    change download time

--- core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/actors/FileDownloader.kt
+++ core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/actors/FileDownloader.kt
@@ -134,7 +134,7 @@ private constructor(
         queue: ArrayBlockingQueue<Double>,
         actionId: String
     ): Timer {
-        return fixedRateTimer("Download Checker ${fileToDownload.fileName}", false, 60_000, 60_000) {
+        return fixedRateTimer("Download Checker ${fileToDownload.fileName}", false, 1_000, 1_000) {
             launch {
                 val progress = inputStream.getProgress()
                 val limit = queue.peek() ?: 1.0
